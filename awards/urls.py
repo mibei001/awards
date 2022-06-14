dc542e83c7947fd957523556fc5540934464ad4d
kevin <kevinmibz6@gmail.com>
2022-06-14 08:15:44 +0300

    register project detail url

--- awards/urls.py
+++ awards/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path("", views.home_page, name='home_page'),
     path("project/post/", views.post, name="post"),
     path("accounts/profile/", views.user_profile, name='profile'),
-
+    path("project/<int:project_id>/", views.project_detail, name="details"),
     path("search/projects/results/", views.search, name="search"),
 ]
