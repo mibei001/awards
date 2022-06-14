84c779ef1a076c1b8bb68f8b6a40df9870d80046
kevin <kevinmibz6@gmail.com>
2022-06-14 07:59:04 +0300

    import rest framework, rest framework views, forms and more to views file

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Projects, Votes, Comments, Profile
+from django.contrib.auth.models import User
+from django.contrib.auth import logout as django_logout
+from django.contrib.auth.decorators import login_required
+from django.http import Http404, HttpResponseRedirect
+from .forms import PostForm, CommentForm, RateForm, UpdateForm
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import ProfileSerializer,  ProjectSerializer
