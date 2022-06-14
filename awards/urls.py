c36f8e509eb86e6d96619e3086a593eb43a81c44
kevin <kevinmibz6@gmail.com>
2022-06-14 08:24:23 +0300

    registering logout,apiview,apiprofile url

--- awards/urls.py
+++ awards/urls.py
@@ -11,4 +11,9 @@ urlpatterns = [
     path("accounts/profile/", views.user_profile, name='profile'),
     path("project/<int:project_id>/", views.project_detail, name="details"),
     path("search/projects/results/", views.search, name="search"),
+    path("logout/", views.logout, name="logout"),
+    path("api/projects/", views.ProjectList.as_view()),
+    path("api/profile/", views.ProfileList.as_view()),
+    path("token/", obtain_auth_token),
+    path("developer/api/", views.apiView, name="api"),
 ]
